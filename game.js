10e617e1aec141b9313567cdf34ec9f4f2a13066
DDubicki <dubickidymitr@gmail.com>
2021-06-25 17:27:53 +0200

    Before work with example

--- game.js
+++ game.js
@@ -3,5 +3,23 @@ initGame();
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
+    compareImages(document.querySelectorAll('.card'))
+}
 
+function compareImages(list){
+    list.forEach(function(element){
+        element.addEventListener('click', compare)
+    })
+}
+
+function compare(event){
+    console.debug(event.target)
+    event.
+//     imageOne = event.target.alt
+//     imageTwo = event.target.alt
+//     if(imageOne == imageTwo) {
+//         console.debug(event.target)        
+//     } else {
+//         console.debug(event.target)        
+//     }
 }
