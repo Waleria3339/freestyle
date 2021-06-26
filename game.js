45cd090a54c445a019d739b349ba1f814b6a2f98
DDubicki <dubickidymitr@gmail.com>
2021-06-26 13:04:05 +0200

    CSS, Get cards, Boarding

--- game.js
+++ game.js
@@ -1,10 +1,55 @@
 initGame();
 
 function initGame() {
+    cards = getCards()
+    boarding(cards)
+    // compareImages(document.querySelectorAll('.card'))
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
-    compareImages(document.querySelectorAll('.card'))
 }
+
+function getCards(){
+    return ["butterfly.png", "butterflyEggs.png", "caterpillar.png", "frog.png", "frogEggs.png", "tadpole.png", "butterfly.png", "butterflyEggs.png", "caterpillar.png", "frog.png", "frogEggs.png", "tadpole.png"]
+}
+
+function boarding(cards){
+    let board = document.getElementById('board_id')
+    for (let i=0; i<cards.length; i++){
+        // console.log(cards[i])
+        document.addEventListener('DOMContentLoaded', function (){
+            let divElement = document.createElement('div');
+            divElement.id = "card"+i;
+            divElement.className = 'card'
+            board.appendChild(divElement)
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function compareImages(list){
     list.forEach(function(element){
@@ -14,12 +59,20 @@ function compareImages(list){
 
 function compare(event){
     console.debug(event.target)
-    event.
 //     imageOne = event.target.alt
 //     imageTwo = event.target.alt
 //     if(imageOne == imageTwo) {
-//         console.debug(event.target)        
+//         console.debug(event.target)
 //     } else {
-//         console.debug(event.target)        
+//         console.debug(event.target)
 //     }
 }
+
+
+// alert(cards[3])
+// console.log(cards)
+
+
+// function revealcards(number){
+//
+// }
