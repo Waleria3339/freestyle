2e310854e2bb2575305c93cb5c56e40c629f8ce6
DDubicki <dubickidymitr@gmail.com>
2021-06-27 11:56:17 +0200

    Project after refactoring

--- game.js
+++ game.js
@@ -50,56 +50,45 @@ function counterBoarding(){
 
 function clickCard(cardNumber){
     let card = document.getElementById('card' + cardNumber)
-    card.addEventListener('click', function () {
-        revealCard(cardNumber)
-    })
+    card.addEventListener('click', function () { revealCard(cardNumber) })
 }
 
 function revealCard(cardNumber){
-
     // opacityValue domyślnie jest 1, po zniknęciu 0
     let opacityValue = $('#card' + cardNumber).css('opacity')
     let image = "url(images/" + cards[cardNumber] + ")";
-    // alert('Opacity: '+opacityValue)
-
     if (opacityValue != 0 && lockCards === false){
-
         lockCards = true;
 
         // Odtwarzanie karty
-        // '#card': szukaj po ID pod nazwą "card"
         $('#card' + cardNumber).css('background-image', image);
         $('#card' + cardNumber).addClass('cardActive');
         $('#card' + cardNumber).removeClass('card');
 
-        if (firstVisibleCard === false) {
-            // first card
-            firstVisibleCard = true;
-            visibleCardNumber = cardNumber;
-            lockCards = false;
-
-        } else {
-            // second card
-
-            if (cards[cardNumber] === cards[visibleCardNumber]){
-                // alert('para')
-                // ustawienie tajmera na 750 mls wywołania funkcji "hideCards"
-                setTimeout(function () { hideCards(cardNumber, visibleCardNumber) }, 750);
-
-            } else {
-                // alert('nie para')
-                setTimeout(function () { restoreCards(cardNumber, visibleCardNumber) }, 1000);
-
-
-            }
-
-            turnCounter ++;
-            $('.score').html('Turn counter: ' + turnCounter);
-            firstVisibleCard = false;
-        }
+        // first card
+        if (firstVisibleCard === false) { checkFirstCard(cardNumber) }
+        // second card
+        else { checkSecondCard(cardNumber) }
     }
-
 }
+
+function checkFirstCard(cardNumber){
+    firstVisibleCard = true;
+    visibleCardNumber = cardNumber;
+    lockCards = false;
+}
+
+function checkSecondCard(cardNumber){
+    if (cards[cardNumber] === cards[visibleCardNumber]){
+        setTimeout(function () { hideCards(cardNumber, visibleCardNumber) }, 750);
+    } else {
+        setTimeout(function () { restoreCards(cardNumber, visibleCardNumber) }, 1000);
+    }
+    turnCounter ++;
+    $('.score').html('Turn counter: ' + turnCounter);
+    firstVisibleCard = false;
+}
+
 
 // usuwanie kart
 function hideCards(actualCardNumber, visibleCardNumber){
@@ -108,10 +97,9 @@ function hideCards(actualCardNumber, visibleCardNumber){
     $('#card' + visibleCardNumber).css('opacity', 0);
     // zmniejszamy ilość nieodkrytych kart o 1
     pairCards--;
-    if(pairCards === 0){
-        $('.board').html('<h1>Congratulations! <br> You win! <br> Done in ' + turnCounter + ' turns<h1>')
-    }
-    // usuwamy blokadę
+    // sprawdzanie wygranej
+    if(pairCards === 0){ $('.board').html('<h1>Congratulations! <br> You win! <br> Done in ' + turnCounter + ' turns<h1>') }
+    // usuwanie blokady
     lockCards = false;
 }
 
@@ -124,6 +112,6 @@ function restoreCards(actualCardNumber, visibleCardNumber){
     $('#card' + visibleCardNumber).css('background-image', 'url(images/background.jpg)');
     $('#card' + visibleCardNumber).addClass('card');
     $('#card' + visibleCardNumber).removeClass('cardActive');
-
+    // usuwanie blokady
     lockCards = false;
 }
